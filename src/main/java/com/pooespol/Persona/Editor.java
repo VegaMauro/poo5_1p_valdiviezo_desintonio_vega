f026f8f1369dba5a84137e6bf9dac953a36ac7f1
VegaMauro <mafevega@espol.edu.ec>
2024-07-03 18:48:38 -0500

    Update Editor.java

--- src/main/java/com/pooespol/Persona/Editor.java
+++ src/main/java/com/pooespol/Persona/Editor.java
@@ -1,5 +1,47 @@
 package com.pooespol.Persona;
 
 public class Editor {
-    
+    protected String journal;
+    private String contrasena;
+    protected usuario rol;
+    protected String user;
+
+    public editor(String journal, String contrasena, String user, usuario rol) {
+        this.journal = journal;
+        this.contrasena = contrasena;
+        this.rol = rol;
+        this.user = user;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContraseña(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public void setJournal(String journal) {
+        this.journal = journal;
+    }
+
+    public String getJournal() {
+        return journal;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setRol(usuario rol) {
+        this.rol = rol;
+    }
+
+    public usuario getRol() {
+        return rol;
+    }
 }
